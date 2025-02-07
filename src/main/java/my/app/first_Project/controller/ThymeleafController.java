7e627cd836521dd5a919919f66941204be09c5fa
hyeseon0105 <hyesun0536@gmail.com>
2025-02-07 15:54:48 +0900

    #7. for 루프 작업 완성

--- src/main/java/my/app/first_Project/controller/ThymeleafController.java
+++ src/main/java/my/app/first_Project/controller/ThymeleafController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDateTime;
 import java.util.*;
@@ -97,6 +98,17 @@ public class ThymeleafController {
         return "/basic/for_opr";
     }
 
+    @GetMapping("/ive")
+    public String details(
+            @RequestParam("name")String name,
+            @RequestParam("age")int age,
+            @RequestParam("addr")String addr,
+            Model model) {
+        model.addAttribute("selectUser",
+                UserData.fromParams(name, age, addr));
+        return "/basic/detail";
+    }
+
     private void addUser(Model model) {
         List<UserData> userList = new ArrayList<>(
                 Arrays.asList(
@@ -108,8 +120,7 @@ public class ThymeleafController {
                         new UserData("레이", 22, "울산")
                 )
         );
-        model.addAttribute(userList);
+        model.addAttribute("userList",userList);
         System.out.println(userList);
-
     }
 }
